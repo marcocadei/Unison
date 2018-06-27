07aa7768dfc290d64d705c29aa3cc6dbe763107d
litchay <pigic116@gmail.com>
2018-06-27 17:21:08 +0200

    Corretto ulteriore bug mi piace Ora l'icona del cuore viene disabilitata ogni volta che si invia una richiesta, in modo che non sia possibile inviare richieste molteplici.

--- public/js/like.js
+++ public/js/like.js
@@ -1,5 +1,7 @@
 function toggleLike(btnLike, idLiked) {
 
+    $(btnLike).addClass("disabledAnchor");
+
     $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -10,14 +12,19 @@ function toggleLike(btnLike, idLiked) {
             liked: idLiked
         },
         function (data, status, xhr) {
-            // Modifica del numero dei like
-            let likesSpan = $(btnLike).next();
-            if (!likesSpan.text().match(/[a-z]/i)) {
-                let likesCount = parseInt(likesSpan.text());
-                let delta = $(btnLike).hasClass("buttonOn") ? -1 : +1;
-                likesSpan.text(likesCount + delta);
-            }
+            if (data.result) {
+                // Modifica del numero dei like
+                let likesSpan = $(btnLike).next();
+                if (!likesSpan.text().match(/[a-z]/i)) {
+                    let likesCount = parseInt(likesSpan.text());
+                    let delta = $(btnLike).hasClass("buttonOn") ? -1 : +1;
+                    likesSpan.text(likesCount + delta);
+                }
 
-            $(btnLike).toggleClass("buttonOn");
-        }, "json");
-};
+                $(btnLike).toggleClass("buttonOn");
+            }
+        }, "json")
+        .always(function () {
+            $(btnLike).removeClass("disabledAnchor");
+        });
+}
