b600926f3f4abece162a39ee9fd1a8f630f03c9e
Isdreavus <isdreavus95@gmail.com>
2018-06-29 14:10:34 +0200

    Corretto messaggio d'errore form di registrazione

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -73,7 +73,7 @@
                                 <label for="usernameSU">Username:</label>
                                 <input type="text" class="form-control" id="usernameSU" name="usernameSU" placeholder="Inserisci username...">
                                 <div class="invalid-feedback">
-                                    Per favore specifica uno username valido: può contenere solamente lettere e/o numeri (lunghezza massima consentita 64 caratteri).
+                                    Per favore specifica uno username valido: (lunghezza massima consentita 64 caratteri, solo caratteri ASCII stampabili).
                                 </div>
                             </div>
                             <div class="form-group">
