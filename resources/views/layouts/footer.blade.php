5ef0e94aca3ed68ef76b9adfdf982c1ba6d6bb61
Isdreavus <isdreavus95@gmail.com>
2018-12-20 09:02:55 +0100

    Risolto problema 5

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,20 +1,17 @@
 <footer class="footer font-weight-normal bg-primary pt-4">
     <div class="container text-center text-md-left">
         <div class="row text-center text-md-left mt-3 pb-3 text-light">
+            <div class="col-sm-12 col-md-4 mx-auto mt-4 pl-3">
+                <img class="img-fluid float-left" src="{{asset('images/bigLogo.png')}}" alt="Logo Unison">
+            </div>
             <div class="col-sm-12 col-md-4 mx-auto mt-4 border-left pl-3">
-                <h1 class="text-uppercase mb-4 font-weight-bold">Unison</h1>
+                <h5 class="text-uppercase mb-4 font-weight-bold">Chi siamo</h5>
                 <p>
                     Ascoltare, condividere e scoprire nuova musica non è mai stato così facile.<br>
                     Immergiti nel mondo Unison e vivi un'esperienza unica.
                 </p>
             </div>
-            <div class="col-sm-12 col-md-3 mx-auto mt-4 border-left pl-3">
-                <h5 class="text-uppercase mb-4 font-weight-bold">Link utili</h5>
-                <p><a class="text-light" href="{{ route('index') }}">Home Page</a></p>
-                <p><a class="text-light" href="{{ route('top50') }}">Top Tracks</a></p>
-                <p><a class="text-light" href="{{ route('register') }}">Registrati</a></p>
-            </div>
-            <div class="col-sm-12 col-md-5 mx-auto mt-4 border-left pl-3">
+            <div class="col-sm-12 col-md-4 mx-auto mt-4 border-left pl-3">
                 <h5 class="text-uppercase mb-4 font-weight-bold">Contatti</h5>
                 <p><i class="fa fa-envelope mr-3"></i><a class="text-light" href="mailto:unison@altervista.org">unison@altervista.org</a></p>
                 @if(!isset($showLinkER) || !$showLinkER)
