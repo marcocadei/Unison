f7db0bdf6b3ee469e84d76c550850cab77460f21
MarcoCadei <m.cadei010@studenti.unibs.it>
2018-12-20 08:53:15 +0100

    Risolto problema 6

--- resources/views/tricol/feed.blade.php
+++ resources/views/tricol/feed.blade.php
@@ -4,3 +4,13 @@
     Feed | {{auth()->user()->username}}
 @endsection
 
+@section('middlecol_header')
+    <div class="container">
+        <div class="row align-items-center justify-content-center profileInfoBackground rounded">
+            <div class="col-12 text-left">
+                <h2 class="boldText wordBreak text-left my-2">Il tuo feed</h2>
+                <p class="wordBreak">Qui puoi ascoltare le tracce pubblicate dagli artisti che segui</p>
+            </div>
+        </div>
+    </div>
+@endsection
